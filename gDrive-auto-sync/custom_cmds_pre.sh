1569d11309f2e813d8e9f29c730043e62ea06a89
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-01-24 23:31:25 +0530

    Added new entry to include the list of all the PPA installed

--- gDrive-auto-sync/custom_cmds_pre.sh
+++ gDrive-auto-sync/custom_cmds_pre.sh
@@ -5,3 +5,4 @@ mkdir -p "${temp_dir_path}"
 
 directory_snapshot "/media/Data/anmol" "${temp_dir_path}/snapshot" "${temp_dir_path}/snapshot_logs"
 comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) > "${temp_dir_path}/manually_installed_apps.txt"
+grep -RoPish "ppa.launchpad.net/[^/]+/[^/ ]+" /etc/apt | sort -u | sed -r 's/\.[^/]+\//:/' > "${temp_dir_path}/installed_ppa_list.txt"
