d5298f0c285de0527c5630f1bca771dd5e70938b
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-03-06 03:13:27 +0530

    Modified the custom_cmd scripts to keep Google Chrome's settings out of the config folder during backup

--- gDrive-auto-sync/custom_cmds_post.sh
+++ gDrive-auto-sync/custom_cmds_post.sh
@@ -1,4 +1,6 @@
 # Commands to be run after invoking the main script
 
-temp_dir_path="/home/anmol/auto-backup-temp"
+temp_dir_path="/home/anmol/.auto-backup-temp"
 rm -r "${temp_dir_path}"
+
+mv /home/anmol/.config.google-chrome /home/anmol/.config/google-chrome
--- gDrive-auto-sync/custom_cmds_pre.sh
+++ gDrive-auto-sync/custom_cmds_pre.sh
@@ -1,8 +1,9 @@
 # Commands to be run before invoking the main script
 
-temp_dir_path="/home/anmol/auto-backup-temp"
+temp_dir_path="/home/anmol/.auto-backup-temp"
 mkdir -p "${temp_dir_path}"
 
 directory_snapshot "/media/Data/anmol" "${temp_dir_path}/snapshot" "${temp_dir_path}/snapshot_logs"
 comm -23 <(apt-mark showmanual | sort -u) <(gzip -dc /var/log/installer/initial-status.gz | sed -n 's/^Package: //p' | sort -u) > "${temp_dir_path}/manually_installed_apps.txt"
 grep -RoPish "ppa.launchpad.net/[^/]+/[^/ ]+" /etc/apt | sort -u | sed -r 's/\.[^/]+\//:/' > "${temp_dir_path}/installed_ppa_list.txt"
+mv /home/anmol/.config/google-chrome /home/anmol/.config.google-chrome
